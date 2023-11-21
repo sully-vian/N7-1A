1013e2430700cfa14ea782da451245927d7751d8
hervyv <vianney.hervy@etu.inp-n7.fr>
2023-11-21 12:25:50 +0100

    ajout Proba

---
+++ 1A/Proba/TP1/decorrelation_colonnes.m
@@ -0,0 +1,15 @@
+% Fonction decorrelation_colonnes (exercice_2.m) 
+
+function I_decorrelee = decorrelation_colonnes(I)
+
+% Initiaisation de l'image décorrélée
+I_decorrelee = I;
+
+% calcul des images réduites
+Id = I(:, 2:end);
+Ig = I(:, 1:end-1);
+
+% Calcul de l'image décorrélée
+I_decorrelee(:, 2:end) = Id - Ig;
+
+end
