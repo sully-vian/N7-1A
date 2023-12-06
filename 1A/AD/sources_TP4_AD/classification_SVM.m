cb50ff65c51affaa9e782c133761daf049b1ba73
hervyv <vianney.hervy@etu.inp-n7.fr>
2023-12-06 10:10:04 +0100

    ajout sources TP4

---
+++ 1A/AD/sources_TP4_AD/classification_SVM.m
@@ -0,0 +1,7 @@
+% fonction classification_SVM (pour l'exercice 1)
+
+function Y_pred = classification_SVM(X,w,c)
+
+
+
+end
---
+++ 1A/AD/sources_TP4_AD/estim_param_SVM_dual.m
@@ -0,0 +1,7 @@
+% fonction estim_param_SVM_dual (pour l'exercice 1)
+
+function [X_VS,w,c,code_retour] = estim_param_SVM_dual(X,Y)
+
+
+
+end
