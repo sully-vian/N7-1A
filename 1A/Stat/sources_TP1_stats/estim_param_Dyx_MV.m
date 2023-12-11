56563522c05cd922044f99d20b21203206e522e7
hervyv <vianney.hervy@etu.inp-n7.fr>
2023-12-11 12:06:30 +0100

    en fait si c'est bon  pour le 1 (pas bis)

--- 1A/Stat/sources_TP1_stats/estim_param_Dyx_MV.m
+++ 1A/Stat/sources_TP1_stats/estim_param_Dyx_MV.m
@@ -4,25 +4,26 @@ function [a_Dyx,b_Dyx,residus_Dyx] = ...
     estim_param_Dyx_MV(x_donnees_bruitees,y_donnees_bruitees,tirages_psi)
 
     % Extracton des données utiles
-    [~, y_G, x_c, y_c] = centrage_des_donnees(x_donnees_bruitees,y_donnees_bruitees);
+    [x_G, y_G, x_c, y_c] = centrage_des_donnees(x_donnees_bruitees,y_donnees_bruitees);
     nb_angles = length(tirages_psi);
 
+    nb_pts = length(x_c);
     tan_psi = tan(tirages_psi);
 
     X_C = repmat(x_c, 1, nb_angles);
     Y_C = repmat(y_c, 1, nb_angles);
 
     % Calcul des termes à minimiser
-    M = Y_C - X_C * tan_psi;
+    M = Y_C - X_C .* repmat(tan_psi', nb_pts, 1);
     M2 = M .^ 2;
     M_sum = sum(M2);
 
-    [val_min, ind_min] = min(M_sum);
+    [~, ind_min] = min(M_sum);
 
     psi_min = tirages_psi(ind_min);
 
     a_Dyx = tan(psi_min);
-    b_Dyx = y_G;
-    residus_Dyx = val_min;
+    b_Dyx = y_G - a_Dyx * x_G;
+    residus_Dyx = M2(:, ind_min);
 
 end
