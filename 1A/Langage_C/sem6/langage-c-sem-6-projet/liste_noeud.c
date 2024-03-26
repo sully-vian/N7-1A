03014967944b3af3ad2988f20c2203333c711044
Vianney HERVY on COMPUTAH <vianney.hervy@etu.inp-n7.fr>
2024-03-26 10:16:27 +0100

    avancement projet C sem6, finir liste_noeud.c

--- 1A/Langage_C/sem6/langage-c-sem-6-projet/liste_noeud.c
+++ 1A/Langage_C/sem6/langage-c-sem-6-projet/liste_noeud.c
@@ -1,8 +1,55 @@
 #define _GNU_SOURCE
 #include "liste_noeud.h"
-#include <stdlib.h>
 #include <math.h>
+#include <stdlib.h>
 
-/* TODO */
+struct _cellule {
+    noeud_id_t noeud;
+    float distance;
+    noeud_id_t precedent;
+};
+typedef struct _cellule _cellule;
 
+struct liste_noeud_t {
+    _cellule cellule;
+    liste_noeud_t *suivant;
+};
 
+liste_noeud_t *creer_liste() {
+    liste_noeud_t *liste;
+    liste->suivant = NULL;
+    liste->cellule.noeud = NO_ID;
+    liste->cellule.distance = 0;
+    liste->cellule.precedent = NO_ID;
+    return liste;
+}
+
+void detruire_liste(liste_noeud_t **liste_ptr) {
+    detruire_liste((*liste_ptr)->suivant);
+    free(*liste_ptr);
+    *liste_ptr = NULL;
+}
+
+bool est_vide_liste(const liste_noeud_t *liste) {
+    return (liste->cellule.noeud == NO_ID) && (liste->suivant == NULL);
+}
+
+bool contient_noeud_liste(const liste_noeud_t *liste, noeud_id_t noeud) {
+    if (liste == NULL) {
+        return false;
+    } else if (liste->cellule.noeud == noeud) {
+        return true;
+    } else {
+        return contient_noeud_liste(liste->suivant, noeud);
+    }
+}
+
+float distance_noeud_liste(const liste_noeud_t *liste, noeud_id_t noeud) {
+    if (liste = NULL) {
+        return INFINITY;
+    } else if (liste->cellule.noeud == noeud) {
+        return liste->cellule.distance;
+    } else {
+        return distance_noeud_liste(liste->suivant, noeud);
+    }
+}
--- 1A/Langage_C/sem6/langage-c-sem-6-projet/liste_noeud.h
+++ 1A/Langage_C/sem6/langage-c-sem-6-projet/liste_noeud.h
@@ -1,7 +1,8 @@
-#include "graphe.h"
 #include <stdbool.h>
+#include "graphe.h"
 
 // TODO : type liste_noeud_t
+typedef struct liste_noeud_t liste_noeud_t;
 
 // TODO : typedef
 
@@ -11,7 +12,7 @@
  * Post-conditions : `r = creer_liste()` => `r != NULL`, `est_vide_liste(r)`
  * @return liste nouvellement créée (de type liste_noeud_t)
  */
-/* TODO : type de retour */ creer_liste();
+liste_noeud_t *creer_liste();
 
 /**
  * detruire_liste : détruit la liste passée en paramètre
@@ -21,7 +22,7 @@
  *
  * @param liste_ptr pointeur sur la liste à détruire
  */
-void detruire_liste(/* TODO : type paramètre */ liste_ptr);
+void detruire_liste(liste_noeud_t **liste_ptr);
 
 /**
  * est_vide_liste : test si la liste passée en paramètre est vide
@@ -31,23 +32,23 @@ void detruire_liste(/* TODO : type paramètre */ liste_ptr);
  * @param liste [in] liste à tester
  * @return vrai ssi la liste ne contient aucun élément
  */
-// TODO : est_vide_liste
+bool est_vide_liste(const liste_noeud_t *liste);
 
 /**
  * contient_noeud_liste : test si le noeud donné appartient à la liste donnée.
- * 
+ *
  * Pré-conditions : liste != NULL
  *
  * @param liste [in] liste à parcourir
  * @param noeud noeud à rechercher
  * @return vrai ssi noeud est dans liste
  */
-bool contient_noeud_liste(const liste_noeud_t* liste, noeud_id_t noeud);
+bool contient_noeud_liste(const liste_noeud_t *liste, noeud_id_t noeud);
 
 /**
  * contient_arrete_liste : test si l'arrête donnée appartient à la liste donnée.
- * L'arrête (source, destination) appartient à la liste ssi destination appartient à liste
- * et si prec(destination) == source.
+ * L'arrête (source, destination) appartient à la liste ssi destination
+ * appartient à liste et si prec(destination) == source.
  *
  * Pré-conditions : liste != NULL
  *
@@ -56,41 +57,48 @@ bool contient_noeud_liste(const liste_noeud_t* liste, noeud_id_t noeud);
  * @param destination noeud destination de l'arrête
  * @return vrai ssi l'arrête (source,destination) est dans liste
  */
-bool contient_arrete_liste(const liste_noeud_t* liste, noeud_id_t source, noeud_id_t destination);
+bool contient_arrete_liste(const liste_noeud_t *liste,
+                           noeud_id_t source,
+                           noeud_id_t destination);
 
 /**
- * distance_noeud_liste : récupère la distance associée au noeud donné dans la liste donnée.
- * Si le noeud n'existe pas dans la liste, retourne `INFINITY`.
+ * distance_noeud_liste : récupère la distance associée au noeud donné dans la
+ * liste donnée. Si le noeud n'existe pas dans la liste, retourne `INFINITY`.
  *
  * Pré-conditions : liste != NULL
- * Post-conditions : `contient_noeud_liste(liste, noeud)` <=> `distance_noeud_liste(liste, noeud) != INFINITY`
+ * Post-conditions : `contient_noeud_liste(liste, noeud)` <=>
+ * `distance_noeud_liste(liste, noeud) != INFINITY`
  *
  * @param liste [in] liste à parcourir
  * @param noeud noeud dont on veut la distance
- * @return distance associée à noeud dans liste ou INFINITY si noeud n'est pas dans liste
+ * @return distance associée à noeud dans liste ou INFINITY si noeud n'est pas
+ * dans liste
  */
-// TODO : distance_noeud_liste
+float distance_noeud_liste(const liste_noeud_t *liste, noeud_id_t noeud);
 
 /**
- * precedent_noeud_liste : récupère le noeud précédent associé au noeud donné dans la liste donnée.
- * Si le noeud n'existe pas, retourne `NO_ID`.
- * 
+ * precedent_noeud_liste : récupère le noeud précédent associé au noeud donné
+ * dans la liste donnée. Si le noeud n'existe pas, retourne `NO_ID`.
+ *
  * Pré-conditions : liste != NULL
- * Post-conditions : `!contient_noeud_liste(liste, noeud)` => `precedent_noeud_liste(liste, noeud) = NO_ID`
+ * Post-conditions : `!contient_noeud_liste(liste, noeud)` =>
+ * `precedent_noeud_liste(liste, noeud) = NO_ID`
  *
  * @param liste [in] liste à parcourir
  * @param noeud noeud dont on veut le précédent
- * @return précédent associé au noeud dans la liste (ou `NO_ID` si noeud n'est pas dans liste)
+ * @return précédent associé au noeud dans la liste (ou `NO_ID` si noeud n'est
+ * pas dans liste)
  */
 // TODO : precedent_noeud_liste
 
 /**
- * min_noeud_liste : trouve le (un) noeud de la liste dont la distance associée est la plus petite,
- * ou renvoie `NO_ID` si la liste est vide.
+ * min_noeud_liste : trouve le (un) noeud de la liste dont la distance associée
+ * est la plus petite, ou renvoie `NO_ID` si la liste est vide.
  *
  * Pré-conditions : liste non NULL
  * Post-conditions : `n = min_noeud_liste(liste) && n != NO_ID` =>
- *   pour tout `n', contient_noeud_liste(liste, n')`, `distance_noeud_liste(liste, n) <= distance_noeud_liste(liste, n')`
+ *   pour tout `n', contient_noeud_liste(liste, n')`,
+ * `distance_noeud_liste(liste, n) <= distance_noeud_liste(liste, n')`
  *
  * @param liste [in] liste à parcourir
  * @return noeud qui minimise la distance, ou `NO_ID` si pas de noeud
@@ -110,8 +118,8 @@ bool contient_arrete_liste(const liste_noeud_t* liste, noeud_id_t source, noeud_
 // TODO : inserer_noeud_liste
 
 /**
- * changer_noeud_liste : modifie les valeurs associées au noeud donné dans la liste donnée.
- * Si le noeud n'est pas dans la liste, il est ajouté.
+ * changer_noeud_liste : modifie les valeurs associées au noeud donné dans la
+ * liste donnée. Si le noeud n'est pas dans la liste, il est ajouté.
  *
  * Pré-conditions : liste != NULL
  * Post-conditions :
@@ -127,15 +135,14 @@ bool contient_arrete_liste(const liste_noeud_t* liste, noeud_id_t source, noeud_
 // TODO : changer_noeud_liste
 
 /**
- * supprimer_noeud_liste : supprime le noeud donné de la liste. Si le noeud n'est pas dans la liste,
- * ne fait rien.
+ * supprimer_noeud_liste : supprime le noeud donné de la liste. Si le noeud
+ * n'est pas dans la liste, ne fait rien.
  *
  * Pré-conditions : liste != NULL
- * Post-conditions : après `supprimer_noeud_liste(liste, n)` : `!contient_noeud_liste(liste, n)`
+ * Post-conditions : après `supprimer_noeud_liste(liste, n)` :
+ * `!contient_noeud_liste(liste, n)`
  *
  * @param liste [in,out] liste à modifier
  * @param noeud noeud à supprimer de liste
  */
 // TODO : supprimer_noeud_liste
-
-
