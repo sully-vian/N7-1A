410b0d25d517684c16aea9a142c0bc2eb89079d4
Vianney HERVY on COMPUTAH <vianney.hervy@etu.inp-n7.fr>
2024-04-03 14:44:56 +0200

    TP Telecom part 1 est finie et beeeelle

--- 1A/Telecom/TP_Telecom.m
+++ 1A/Telecom/TP_Telecom.m
@@ -3,11 +3,10 @@
 %%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Étude d'une chaine de transmission en bade de base %%%
+%%% Étude d'une chaîne de transmission en bade de base %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-clear all
-
+% constantes
 Fe = 24000; % feq ech en Hz
 Te = 1 / Fe; % période ech en s
 Rb = 3000; % debit binaire en bit/s
@@ -20,96 +19,51 @@ N = 500; % nb ech
 
 close all
 
-%%%% Modulateur 1 %%%%
+mod1 = struct('Ns', Tb / Te, 'bits', randi([0, 1], 1, N), 'mapping', [-1, 1], 'filtre', ones(1, Tb / Te), 'color', 'blue');
 
-% params
-Ns1 = Tb / Te; % Nb ech
-tps1 = linspace(0, N / Rb, N * Ns1); % échelle de temps
+mod2 = struct('Ns', 2 * Tb / Te, 'bits', randi([0, 3], 1, N), 'mapping', [-3, -1, 1, 3], 'filtre', ones(1, 2 * Tb / Te), 'color', 'red');
 
-% création signal + mapping
-bits1 = randi([0, 1], 1, N); % générer bits
-a1 = bits1 * 2 - 1; % mapping 1->1 et 0->-1
+mod3 = struct('Ns', Tb / Te, 'bits', randi([0, 1], 1, N), 'mapping', [-1, 1], 'filtre', rcosdesign(0.5, N, Tb / Te), 'color', 'green');
 
-%filtre
-diracs1 = kron(a1, [1 zeros(1, Ns1 - 1)]); % somme diracs
-h1 = ones(1, Ns1); % filtre de réponse impulsionnelle rectangulaire
+modulateurs = [mod1, mod2, mod3];
 
-% sortie
-s1 = filter(h1, 1, diracs1);
-s1 = reshape(s1', 1, numel(s1));
-Sx1 = pwelch(s1, [], [], [], Fe, "twosided"); % DSP
+for i = 1:3
+    mod = modulateurs(i);
+    [s, Sx, tps, freq] = moduler(mod.Ns, mod.bits, mod.mapping, mod.filtre, N, Rb, Fe);
 
-freq1 = linspace(-Fe / 2, Fe / 2, length(Sx1)); % échelle freq
+    subplot(2, 2, i)
+    plot(tps, s, mod.color)
+    title("Modulateur " + i + " - bits envoyés")
+    xlabel("temps")
 
-figure("name", "Modulation 1")
-subplot(2, 1, 1)
-plot(tps1, s1);
-title("bits émis")
-xlabel("temps")
+    subplot(2,2,4)
+    semilogy(freq, fftshift(Sx), mod.color)
+    hold on
+end
 
-subplot(2, 1, 2)
-semilogy(freq1, fftshift(Sx1))
-title("DSP")
+title("Superposition des DSP")
 xlabel("fréquence")
+legend("modulateur 1", "modulateur 2", "modulateur 3")
 
-%%%% Modulateur 2 %%%%
+function [s, Sx, tps, freq] = moduler(Ns, bits, mapping, h, N, Rb, Fe)
+    tps = linspace(0, N / Rb, N * Ns); % échelle de temps
+    a = mapping(bits + 1); % +1 car MATLAB indexe à 1
+    diracs = kron(a, [1 zeros(1, Ns - 1)]); % somme diracs
+    s = filter(h, 1, diracs);
+    s = reshape(s', 1, numel(s));
+    Sx = pwelch(s, [], [], [], Fe, "twosided"); % DSP
+    freq = linspace(-Fe / 2, Fe / 2, length(Sx)); % échelle freq
+end
 
-% params
-Ns2 = 2 * Ns1; % Nb ech
-tps2 = linspace(0, N / Rb, N * Ns2); % échelle de temps
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Étude des interférances entre symbole - Critère de Nyquist %%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% création signal + mapping
-bits2 = randi([0, 3], 1, N); % générer bits
-mapping = [-3, -1, 1, 3];
-a2 = mapping(bits2 + 1); % +1 car MATLAB indexe à 1
+% % TODO
 
-% filtre
-diracs2 = kron(a2, [1 zeros(1, Ns2 - 1)]); % somme diracs
-h2 = ones(1, Ns2); % filtre de réponse impulsionnelle rectangulaire
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Étude de l'impact d'un canal à bruit additif, blanc et Gaussien %%
+% %% - Efficacité en puissance                                       %%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% sortie
-s2 = filter(h2, 1, diracs2);
-s2 = reshape(s2', 1, numel(s2));
-
-Sx2 = pwelch(s2, [], [], [], Fe, "twosided"); % DSP
-
-freq2 = linspace(-Fe / 2, Fe / 2, length(Sx2)); % échelle freq
-
-figure("name", "Modulation 2")
-subplot(2, 1, 1)
-plot(tps2, s2);
-title("bits émis")
-xlabel("temps")
-
-subplot(2, 1, 2)
-semilogy(freq2, fftshift(Sx2))
-title("DSP")
-xlabel("fréquence")
-
-%%%% Modulateur 3 %%%%
-
-% TODO
-
-%%%% Superposition %%%%
-
-figure("name", "superposition des DSP")
-semilogy(freq1, fftshift(Sx1))
-hold on
-semilogy(freq2, fftshift(Sx2), 'r')
-% plot(freq3, Sx3)
-
-% en rouge c'est mieux parce que l'énergie est concentrée sur une petite
-% bande de fréquence
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Étude des interférances entre symbole - Critère de Nyquist %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% TODO
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Étude de l'impact d'un canal à bruit additif, blanc et Gaussien %%
-%% - Efficacité en puissance                                       %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% TODO
+% % TODO
