0ada8f359e5d2d89fa4398b211036205613c1bef
hervyv <vianney.hervy@etu.inp-n7.fr>
2024-04-25 09:55:01 +0200

    TP3 Telecom pafini, on peut clean toussa

---
+++ 1A/Telecom/TP/TP1_Etude_Modulateur.m
@@ -0,0 +1,66 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% TP1 - Étude de modulateurs en bade de base - Efficacité spectrale %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% constantes
+Fe = 24000; % feq ech en Hz
+Te = 1 / Fe; % période ech en s
+Rb = 3000; % debit binaire en bit/s
+Tb = 1 / Rb;
+N = 500; % nb ech
+
+close all
+
+mod1 = struct( ...
+    'Ns', Tb / Te, ...
+    'bits', randi([0, 1], 1, N), ...
+    'mapping', [-1, 1], ...
+    'filtre', ones(1, Tb / Te), ...
+    'color', 'blue' ...
+);
+
+mod2 = struct( ...
+    'Ns', 2 * Tb / Te, ...
+    'bits', randi([0, 3], 1, N), ...
+    'mapping', [-3, -1, 1, 3], ...
+    'filtre', ones(1, 2 * Tb / Te), ...
+    'color', 'red' ...
+);
+
+mod3 = struct( ...
+    'Ns', Tb / Te, ...
+    'bits', randi([0, 1], 1, N), ...
+    'mapping', [-1, 1], ...
+    'filtre', rcosdesign(0.5, N, Tb / Te), ...
+    'color', 'green' ...
+);
+
+modulateurs = [mod1, mod2, mod3];
+
+for i = 1:3
+    mod = modulateurs(i);
+    [s, Sx, tps, freq] = moduler(mod.Ns, mod.bits, mod.mapping, mod.filtre, N, Rb, Fe);
+
+    % subplot(2, 2, i)
+    % plot(tps, s, mod.color)
+    % title("Modulateur " + i + " - bits envoyés")
+    % xlabel("temps")
+
+    % subplot(2, 2, 4)
+    semilogy(freq, fftshift(Sx), mod.color)
+    hold on
+end
+
+title("Superposition des DSP")
+xlabel("fréquence")
+legend("modulateur 1", "modulateur 2", "modulateur 3")
+
+function [s, Sx, tps, freq] = moduler(Ns, bits, mapping, h, N, Rb, Fe)
+    tps = linspace(0, N / Rb, N * Ns); % échelle de temps
+    a = mapping(bits + 1); % +1 car MATLAB indexe à 1
+    diracs = kron(a, [1 zeros(1, Ns - 1)]); % somme diracs
+    s = filter(h, 1, diracs);
+    s = reshape(s', 1, numel(s));
+    Sx = pwelch(s, [], [], [], Fe, "twosided"); % DSP
+    freq = linspace(-Fe / 2, Fe / 2, length(Sx)); % échelle freq
+end
---
+++ 1A/Telecom/TP/TP2_Etude_Interference.m
@@ -0,0 +1,69 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% TP2 - Étude des interférences entre symbole - Critère de Nyquist %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% constantes
+Fe = 24000; % feq ech en Hz
+Te = 1 / Fe; % période ech en s
+Rb = 3000; % debit binaire en bit/s
+Tb = 1 / Rb; % s/bit
+N = 100; % nb ech
+
+duree = N / Rb;
+
+Ns = Tb / Te; % symb / bit
+
+% Ns "temporel" = 0.0003s
+
+close all
+
+% émission
+bits = randi([0, 1], 1, N);
+mapping = [-1, 1];
+a = mapping(bits + 1);
+diracs = kron(a, [1 zeros(1, Ns - 1)]); % somme diracs
+h = ones(1, Ns);
+s = filter(h, 1, diracs);
+tps = linspace(0, duree, length(s)); % échelle temporelle
+
+figure( ...
+    "Name", "Signaux émis et en sortie du filtre de réception", ...
+    "NumberTitle", "off")
+plot(s, "blue");
+
+% sortie de réception
+hr = h;
+s = filter(hr, 1, s);
+
+hold on
+plot(s, "red");
+xlabel("temps")
+ylabel("valeur du symbole")
+str = {"retard de Ts", "Ts entre chaque instant d'échantillonnage"};
+text(0.0005, 6, str)
+
+% décodage
+n0 = 3;
+y_echant = s(n0:Ns:end);
+BitsRecuperes = (sign(y_echant) + 1) / 2; % demapping
+erreur = abs(bits - BitsRecuperes);
+TEB = mean(erreur)
+
+figure( ...
+    "Name", "Réponse impulsionnelle globale de la chaine de transmission", ...
+    "NumberTitle", "off")
+g = conv(h, h);
+plot(g);
+xlabel("????")
+ylabel("filtre global h*hr")
+
+% diagramme de l'oeil
+figure("Name", "diagramme de l'oeil", "NumberTitle", "off")
+s_tronque = s(Ns+1:end);
+plot(reshape(s_tronque, Ns, length(s_tronque)/Ns))
+
+% explication: 4 valeurs possibles pour deuxc bits (symboles) donc 4 transitions
+
+% instant optimal: 8 parce qu'alors, 1 seul symbole pris en compte
+
+% décodage
